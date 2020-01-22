198b25480355c5d9ce491af67ea82cc5021e0a85
Blake <blake.e.richey@gmail.com>
2020-01-21 18:31:21 -0600

    Removed unnecessary comments. Will modify parents removal later.

--- detection/detect.py
+++ detection/detect.py
@@ -4,49 +4,43 @@ import matplotlib.pyplot as plt
 
 def detection(img):
   '''
-    returns number of objects found and their images
+    returns if an object was found and then returns the closest obj to the camera
   '''
   e1 = cv2.getTickCount()
   img = cv2.resize(img, None, fx=.2, fy=.2, interpolation=cv2.INTER_AREA)
-  # print("Size:", img.size)
-  thresh = 0.03*img.size
-  kernel = np.ones((5,5), np.uint8)
+  thresh = 0.03*img.size #minimum size for object to be detected
   blur = cv2.medianBlur(img, 3)
   edges = cv2.Canny(blur, 150, 200)
 
   edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, np.ones((5,5), np.uint8), iterations=1)
-  contours, hierarchy = cv2.findContours(edges.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+  image, contours, hierarchy = cv2.findContours(edges.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-  objs = []
-  border_id = None
+  objs = [] #cropped images
+  coords = [] #x,y,w,h locations
+  border_id = None #will be used for primary parent identifier
   for i, c in enumerate(contours):
     x,y,w,h = cv2.boundingRect(c) #get bounding box
     if w*h >= thresh: #if bounding box big enough
       parent = hierarchy[0][i][3]
-      # print('heir', hierarchy[0][i], parent)
-      # if border_id == None:
-      #   border_id = i
-      # if parent == border_id:
-      # print("W, H", w, h, w*h)
-      # print('X, Y', (x, y), '\n\n')
+      #INSERT LOGIC TO REMOVE PRIMARY PARENT HERE
       objs.append(img[y:y+h, x:x+w])
-      # img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,255),2)
+      coords.append((len(coords),x,y,w,h))
+  coords = sorted(coords, key=lambda x: x[1]) #sort by x location (closest box to left is closest obj)
   
-  return len(objs), objs
-
+  res = None
+  if len(objs) > 1:
+    res = objs[coords[1][0]] #ignore primary parent, so coords[1]. [0] gets index number
 
   e2 = cv2.getTickCount()
   time = (e2 - e1)/ cv2.getTickFrequency()
   print("Time taken:", time)
+  return res is not None, res
 
-  # cv2.imshow("Image", img)
-  # cv2.imshow("edges", edges)
-  # cv2.waitKey(0)
-  # cv2.destroyAllWindows()
-
-img = cv2.imread('wb.jpg', cv2.IMREAD_COLOR)
-num, imgs = detection(img)
-for i, image in enumerate(imgs):
-  cv2.imshow(str(i), image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+img = cv2.imread('empty.jpg', cv2.IMREAD_COLOR)
+obj_found, obj = detection(img)
+if obj_found:
+  cv2.imshow("Object", obj)
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
+else:
+  print('No object found')
