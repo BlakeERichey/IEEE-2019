07600862992e83944f046adddc08d2c33af1d862
Gaddiel Morales <Gaddiel Morales>
2020-01-31 14:48:08 -0600

    Added code for getting error margin of IMU

--- Ard1/Ard1.ino
+++ Ard1/Ard1.ino
@@ -3,6 +3,8 @@
 // an MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
 MPU9250 IMU(Wire,0x68);
 int status;
+double accelError[3];
+double gyroError[3];
 
 void setup() {
   // serial to display data
@@ -26,21 +28,30 @@ void setup() {
   IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_20HZ);
   // setting SRD to 19 for a 50 Hz update rate
   IMU.setSrd(19);
-
+  
+  IMU.readSensor();
+  //margin of error is .025 m/(s^2);
+  accelError[3] = {IMU.getAccelX_mss(),IMU.getAccelY_mss(),IMU.getAccelZ_mss()};
+  gyroError[3] = {Double(IMU.getGyroX_rads()),DOuble(IMU.getGyroY_rads()),Double(IMU.getGyroZ_rads())};
 }
 
 void loop() {
   // read the sensor
   IMU.readSensor();
-  time1 = micros();
+  
+  double accel0[3] = {IMU.getAccelX_mss()-accelError[0],IMU.getAccelY_mss()-accelError[1],IMU.getAccelZ_mss()-accelError[2]};
+  double gyro0[3] = {IMU.getGyroX_rads()-gyroError[0],IMU.getGyroY_rads()-gyroError[1],IMU.getGyroZ_rads()-gyroError[2]};
+  double time1 = micros();
 
+  //while no input to read 
   do{
     
     //calculate displacement between last point and current point
-
+    
+    
     //calculate angle change between last angle and current angle
   
-  }while();
+  }while(!(Serial.available()>0));
   
 
   
@@ -52,7 +63,6 @@ void recieveData(){
   if (Serial.available() > 0){//is a char available?
     char msg = Serial.read();      //get the character
     //Serial.println(msg);
-    printLCD("value recieved",double(msg),true);
   }
 }
 
