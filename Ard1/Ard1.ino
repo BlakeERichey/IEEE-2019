5d071a7ba8ea22fdf9863f8b88db05d880b348ac
Blake <blake.e.richey@gmail.com>
2020-02-07 18:10:09 -0700

    Added code for claw and flippers.

--- Ard1/Ard1.ino
+++ Ard1/Ard1.ino
@@ -8,65 +8,42 @@ String active = "pi";
 
 //servo pins
 const int clawPin = 3;
+const int flippers = 2;
 
 //variables
 bool clawOpen = true;
+bool flippersOpen = true;
+Servo Claw;
+Servo Flipper;
 void setup() {
-  Servo Claw;
   Claw.attach(clawPin);
+  Flipper.attach(flippers);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  toggleClaw();
+  toggleFlippers();
   delay(2000);
-  // if(active == "pi"){
-  //   sendData(NAME);
-  //   delay(100);
-  //   recieveData();
-  // }else{
-  //   recieveData();
-  // }
-}
-
-void recieveData(){
-  if (Serial.available() > 0){//is a char available?
-    active = "ard";                //sets ard to active
-    char msg = Serial.read();      //get the character
-    runCommand(int(msg));
-  }
-}
-
-void sendData(int val){
-  Serial.println(val);
 }
 
 void toggleClaw(){
   if (clawOpen) {
-    claw.write(180);
+    Claw.write(180);
   }else{
-    claw.write(0);
+    Claw.write(0);
   }
   clawOpen = !clawOpen;
 }
 
-void runCommand(int action){
-  switch(action%48){
-    case 0:
-      stopMotors();
-      break;
-    case 1:
-      linear(true); //move forward
-      break;
-    case 2:
-      linear(false); //move backward
-      break;
-    case 3:
-      rotate(true); //rotate right
-      break;
-    case 4:
-      rotate(false); //rotate left
-      break;  
+toggleFlippers(){
+  if(flippersOpen){
+    for(int i = 0; i<180; i+=5){
+      Flipper.write(i+1);
+      delay(200);
+    }
+  }{
+    Flipper.write(0);
   }
+  flippersOpen = !flippersOpen;
 }
